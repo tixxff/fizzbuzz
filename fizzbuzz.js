d324f1d5e64b858529b20028b3e50d22fa74c15f
tixxff <awatif5166@gmail.com>
2019-09-21 16:32:22 +0700

    add new fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,33 @@
 class FizzBuzz {
   static say(number) {
-    return 1;
+    // if (number % 15 === 0) {
+    //   return "FizzBuzz";
+    // } else if (number % 3 === 0) {
+    //   return "Fizz";
+    // } else if (number % 5 === 0) {
+    //   return "Buzz";
+    // } else {
+    //   return number;
+    // }
+    switch (true) {
+      case this.mod(number, 15) === 0:
+        return "FizzBuzz";
+
+      case this.mod(number, 3) === 0:
+        return "Fizz";
+
+      case this.mod(number, 5) === 0:
+        return "Buzz";
+    }
   }
+
+  //   static mod(number, base) {
+  //     while (number >= base) {
+  //       number -= base;
+  //     }
+  //     return number;
+
+  //return number - Math.floor(number / base)*base;
 }
+
 module.exports = FizzBuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,8 +1,43 @@
 const FizzBuzz = require("./fizzbuzz");
 
 describe("Test FizzBuzz", () => {
-  test("Test 1 expected 1", () => {
-    const result = FizzBuzz.say(1);
-    expect(result).toEqual(1); //Assert Function
+  //   test("Test 1 expected 1", () => {
+  //     const result = FizzBuzz.say(1);
+  //     expect(result).toEqual(1); //Assert Function
+  //   });
+
+  //   test("Test 2 expected 2", () => {
+  //     const result = FizzBuzz.say(2);
+  //     expect(result).toEqual(2); //Assert Function
+  //   });
+
+  //   test("Test 3 expected Fizz", () => {
+  //     const result = FizzBuzz.say(3);
+  //     expect(result).toEqual("Fizz"); //Assert Function
+  //   });
+
+  //   test("Test 4 expected 4", () => {
+  //     const result = FizzBuzz.say(4);
+  //     expect(result).toEqual(4); //Assert Function
+  //   });
+
+  //   test("Test 5 expected 100", () => {
+  //     const result = FizzBuzz.say(100);
+  //     expect(result).toEqual("Buzz"); //Assert Function
+  //   });
+
+  //   test("Test 5 expected Buzz", () => {
+  //     const result = FizzBuzz.say(5);
+  //     expect(result).toEqual("Buzz"); //Assert Function
+  //   });
+
+  //   test("Test 6 expected FizzBuzz", () => {
+  //     const result = FizzBuzz.say(15);
+  //     expect(result).toEqual("FizzBuzz"); //Assert Function
+  //   });
+
+  test("Test 5%5 expected 0", () => {
+    const result = FizzBuzz.mod(5, 5);
+    expect(result).toEqual(0); //Assert Function
   });
 });
