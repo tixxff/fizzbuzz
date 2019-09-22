913e85ddc96487375163f63d3c7c0582bab3bb93
tixxff <awatif5166@gmail.com>
2019-09-22 09:23:39 +0700

    edit fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,33 +1,23 @@
 class FizzBuzz {
   static say(number) {
-    // if (number % 15 === 0) {
-    //   return "FizzBuzz";
-    // } else if (number % 3 === 0) {
-    //   return "Fizz";
-    // } else if (number % 5 === 0) {
-    //   return "Buzz";
-    // } else {
-    //   return number;
-    // }
     switch (true) {
       case this.mod(number, 15) === 0:
         return "FizzBuzz";
-
       case this.mod(number, 3) === 0:
         return "Fizz";
-
       case this.mod(number, 5) === 0:
         return "Buzz";
+      default:
+        return number;
     }
   }
 
-  //   static mod(number, base) {
-  //     while (number >= base) {
-  //       number -= base;
-  //     }
-  //     return number;
-
-  //return number - Math.floor(number / base)*base;
+  static mod(number, base) {
+    // while (number >= base) {
+    //   number -= base;
+    // }
+    // return number;
+    return number - Math.floor(number / base) * base;
+  }
 }
-
 module.exports = FizzBuzz;
